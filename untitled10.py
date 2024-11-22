f0279f86675e5e3e60bf643b396922635c9ee513
palomapaine1 <palomapaine4@gmail.com>
2024-11-22 17:36:17 -0300

    Update untitled10.py

--- untitled10.py
+++ untitled10.py
@@ -68,7 +68,7 @@ if df is not None:
     columnas = st.multiselect('Selecciona las columnas a visualizar', df_cleaned.columns.tolist(), default=df_cleaned.columns.tolist())
     df_seleccionado = df_cleaned[columnas]
     #filtrar las filas en función de valores en una columna numérica
-    columna_filtro = 'population'
+    columna_filtro = 'poblacion'
     
 
 
