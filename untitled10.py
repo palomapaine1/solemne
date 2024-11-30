575dc20fea4822ca7a5286434c274375bdc792f4
palomapaine1 <palomapaine4@gmail.com>
2024-11-30 01:56:34 -0300

    Update untitled10.py

--- untitled10.py
+++ untitled10.py
@@ -5,9 +5,9 @@ import requests
 import streamlit as st
 import io
 
-
-
-#Visualización Interactiva de Datos en Tiempo Real con Streamlit y API REST
+#titulo de la aplicacion
+st.title('Visualización Interactiva de Datos en 
+Tiempo Real con Streamlit y API REST')
 def obtener_datos_paises():
     url = 'https://raw.githubusercontent.com/jxnscv/Programacion/main/all.json'
     respuesta = requests.get(url)
